92176c7f31a6e492e2fda4b9f19060b512dcdad7
xlzy <1017866168@qq.com>
2020-07-10 21:49:43 +0800

    add: 云开发配置

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 //app.js
 App({
   onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        env: 'a88888a98-1d9f1',
+        traceUser: false,
+      })
+    }
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -41,6 +49,7 @@ App({
     currentIndex: 0,
     fullScreen: false,
     songlist: [],
+    likeMusics: [],
     playing: false,
     innerAudioContext: null
   }
